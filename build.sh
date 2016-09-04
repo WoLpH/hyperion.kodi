4f4fba3c73ed2cfd76099da88b3a1aab2915cf4f
Rick van Hattem <Wolph@wol.ph>
2016-09-04 14:41:10 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-rm ~/workspace/script.service.hyperion
-ln -s "$PWD" ~/workspace/script.service.hyperion
-cd ~/workspace/
+ln -s "$PWD" ~/script.service.hyperion
+cd ~/
 rm -f script.service.hyperion.zip
 zip -x '*/.*' -r script.service.hyperion.zip script.service.hyperion
-
+rm ~/script.service.hyperion
